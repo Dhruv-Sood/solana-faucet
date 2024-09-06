b58e92f4e1c6ba31198b9bd950c6dcd929b51104
Dhruv Sood <dhruvsoodboss@gmail.com>
2024-09-06 23:38:27 +0530

    Update padding on Faucet

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -2,7 +2,7 @@ import { WalletCard } from "./wallet-card"
 
 const Faucet = ({publicKey}:any) => {
   return (
-    <div className="min-h-[100vh] flex justify-center items-center w-full">
+    <div className="min-h-[100vh] flex justify-center items-center w-full p-4">
         <WalletCard publicKey={publicKey}/>
     </div>
   )
